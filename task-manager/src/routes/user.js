dcbdfb9f5e1c2a7467ea93a027434196c643658f
roberts98 <robertsidzinka@interia.pl>
2019-10-20 21:46:19 +0200

    login route

--- task-manager/src/routes/user.js
+++ task-manager/src/routes/user.js
@@ -16,6 +16,19 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+
+    res.send(user);
+  } catch (error) {
+    res.status(400).send();
+  }
+});
+
 router.get('/', async (req, res) => {
   try {
     const users = await User.find();
